67d7fefa2ca9cfb95f9168b9961d009fcf7eb309
MatiasTortoriello <matiastortoriello1991@gmail.com>
2025-01-14 19:20:03 -0300

    Comienzo TP Final

---
+++ clase-9/src/main/java/com/coderhouse/entities/Cliente.java
@@ -0,0 +1,126 @@
+package com.coderhouse.entities;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+
+@Table(name = "Clientes")
+
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "Nombre")
+	private String nombre;
+	
+	@Column(name = "Apellido")
+	private String apellido;
+	
+	@Column(unique = true, nullable = false, name = "DNI")	
+	private Integer dni;
+	
+	@Column(name = "Direccion")	
+	private String direccion;
+	
+	
+	@ManyToMany(mappedBy = "cliente", fetch = FetchType.EAGER)
+	private List<Producto> productos = new ArrayList<>();
+	
+	private LocalDateTime createdAt;
+	
+	public Cliente() {
+		super();
+
+	}
+	
+	public Cliente(String nombre, String apellido, Integer dni, String direccion) {
+		this();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.direccion = direccion;
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Integer getDni() {
+		return dni;
+	}
+
+	public void setDni(Integer dni) {
+		this.dni = dni;
+	}
+	
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", direccion="
+				+ direccion + ", productos=" + productos + ", createdAt=" + createdAt + "]";
+	}
+	
+	
+	
+	
+	
+}
